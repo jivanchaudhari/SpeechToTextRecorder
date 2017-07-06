6af496186df9cc045a9a8c8a39a166daf0c074a8
prajakta <prajakta@iternia.com>
2017-07-06 12:48:04 +0530

    Initial Commit

---
+++ JCRecorder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JCRecorder
+//
+//  Created by Mindrose on 06/07/17.
+//  Copyright © 2017 Mindrose. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JCRecorder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JCRecorder
+//
+//  Created by Mindrose on 06/07/17.
+//  Copyright © 2017 Mindrose. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
